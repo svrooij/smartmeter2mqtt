a4af52ba1149b82cd1f0f3e5d31bfbe048f2e1ed
Stephan van Rooij <github@svrooij.nl>
2019-10-06 17:27:25 +0200

    test: Added test for HttpOutput

--- tests/output-tests.js
+++ tests/output-tests.js
@@ -4,6 +4,7 @@ const P1ReaderEvents = require('../lib/p1-reader-events')
 const DebugOutput = require('../lib/output/debug-output')
 const TcpServer = require('../lib/output/tcp-server')
 const WebServer = require('../lib/output/web-server')
+const HttpOutput = require('../lib/output/http-output')
 const EventEmitter = require('events')
 
 describe('DebugOutput', function () {
@@ -11,7 +12,6 @@ describe('DebugOutput', function () {
     const fakeReader = new EventEmitter()
     const debugOutput = new DebugOutput()
     debugOutput.start(fakeReader)
-    assert.strictEqual(fakeReader.listenerCount(P1ReaderEvents.ParsedResult), 1, 'Debug output not subscribed to ParsedResult event')
     assert.strictEqual(fakeReader.listenerCount(P1ReaderEvents.ParsedResult), 1, 'Debug output not subscribed to ParsedResult event')
     assert.strictEqual(fakeReader.listenerCount(P1ReaderEvents.ErrorMessage), 1, 'Debug output not subscribed to ErrorMessage event')
     debugOutput.close()
@@ -39,5 +39,15 @@ describe('WebServer', function () {
     const webServer = new WebServer()
     webServer.start(fakeReader, { startServer: false })
     assert.strictEqual(fakeReader.listenerCount(P1ReaderEvents.ParsedResult), 1, 'WebServer not subscribed to ParsedResult event')
+  })
+})
+
+describe('HttpOutput', function () {
+  it('Should subscribe to Parsed result event', function () {
+    const fakeReader = new EventEmitter()
+    const httpOutput = new HttpOutput()
+    httpOutput.start(fakeReader)
+    assert.strictEqual(fakeReader.listenerCount(P1ReaderEvents.ParsedResult), 1, 'HttpOutput not subscribed to ParsedResult event')
+    httpOutput.close()
   })
 })
